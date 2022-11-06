3721ba8f85f6f1ce4b5cb0bf392a3cd39b16b2d6
Freddy <freddy@Zipcoders-MacBook-Pro-7.local>
2022-11-06 09:54:28 -0500

    Checkpoint

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,37 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Student {
+    String firstName;
+    String lastName;
+    Double[] examScores;
+
+    Student(String firstName, String lastName, Double[]examScores) {
+        this.examScores = examScores;
+
+    }
+    String getFirstName() {
+        return firstName;
+    }
+    void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    String getLastName() {
+        return lastName;
+    }
+    void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    Double[] getExamScores() {
+        return examScores;
+    }
+    Double[] addExamScores(double examScore) {
+        ArrayList<Double> temp = new ArrayList<>(Arrays.asList(examScores));
+        temp.add(examScore);
+        examScores = (Double[]) temp.toArray();
+        return examScores;
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,25 @@
 package io.zipcoder;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.*;
 
 public class StudentTest {
 
+    String firstName = "Freddy";
+    String lastName = "Mintarja";
+    Double[] examScores = {90.5};
+    Student student = new Student(firstName, lastName, examScores);
+
+//    @BeforeEach
+//    void setUp() {
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//    }
+    @Test
+    public void getFirstNameTest() {
+        student.setFirstName("Freddy");
+        Assert.assertEquals(firstName, student.getFirstName());
+    }
 }
