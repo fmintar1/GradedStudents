1fe6f954cd79d54ab3902ec87e2efa151105ea91
Freddy <freddy@Zipcoders-MacBook-Pro-7.local>
2022-11-06 22:22:26 -0500

    Fran Stalinovskovichdavidovitchsky

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,37 +1,64 @@
 package io.zipcoder;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Student {
-    String firstName;
-    String lastName;
+    //Field of String firstName, String lastName, and Double[] examScores;
+    String firstName, lastName;
     Double[] examScores;
 
+    //Constructor of Student with parameter of (String firstName, String lastName, Double[] examScores)
     Student(String firstName, String lastName, Double[]examScores) {
-        this.examScores = examScores;
 
+        //Assigning examScores directly
+        this.examScores = examScores;
     }
     String getFirstName() {
         return firstName;
     }
     void setFirstName(String firstName) {
+
+        //Assigning firstName passed to firstName field
         this.firstName = firstName;
     }
     String getLastName() {
         return lastName;
     }
     void setLastName(String lastName) {
+
+        //Assigning lastName passed to lastName field
         this.lastName = lastName;
     }
     Double[] getExamScores() {
         return examScores;
     }
     Double[] addExamScores(double examScore) {
+
+        //Converting Array to ArrayList temp for methods modification
         ArrayList<Double> temp = new ArrayList<>(Arrays.asList(examScores));
+
+        //Adding examScore passed by the parameter to the ArrayList temp
         temp.add(examScore);
-        examScores = (Double[]) temp.toArray();
+
+        //Assigning the newly updated ArrayList back to examScores Array and return it
+        return examScores = temp.toArray(new Double[0]);
+    }
+    int getNumberOfExamsTaken() {
+        //Returning the length of examScores
+        return examScores.length;
+    }
+    Double[] setExamScores(int examNumber, double newScore) {
+
+        //Create a loop to read through all examScores
+        for (int i = 0; i < examScores.length; i++) {
+
+            //If the examNumber-1 matches where the index is
+            if(i == (examNumber-1)) {
+
+                //Update the examScore index i
+                examScores[i] = newScore;
+            }
+        }
+        //Return the newly updated examScores
         return examScores;
     }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -3,11 +3,13 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.*;
 
+import java.util.Arrays;
+
 public class StudentTest {
 
-    String firstName = "Freddy";
-    String lastName = "Mintarja";
-    Double[] examScores = {90.5};
+    static String firstName;
+    String lastName;
+    Double[] examScores = { };
     Student student = new Student(firstName, lastName, examScores);
 
 //    @BeforeEach
@@ -18,8 +20,88 @@ public class StudentTest {
 //    void tearDown() {
 //    }
     @Test
-    public void getFirstNameTest() {
-        student.setFirstName("Freddy");
-        Assert.assertEquals(firstName, student.getFirstName());
+    public void setFirstNameTest() {
+        //Given a new first name
+        firstName = "Fran";
+
+        //When setFirstName() method is called with parameter (firstName)
+        student.setFirstName(firstName);
+        String expected = firstName;
+        String actual = student.getFirstName();
+
+        //Then firstName should equal to getFirstName(), proving setFirstName() and getFirstName() works
+        System.out.println(expected + " equals to " + actual);
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void setLastNameTest() {
+        //Given a new last name (Dodge, Duck, Dip, Dive, and Dodge)
+        String lastName = "Stalinovskovichdavidovitchsky";
+
+        //When setLastName() method is called with parameter (lastName)
+        student.setLastName(lastName);
+        String expected = lastName;
+        String actual = student.getLastName();
+
+        //Then lastName should equal to getFirstName(), proving setFirstName() & getFirstName() works
+        System.out.println(expected + " equals to " + actual);
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void getExamScoresTest() {
+        //Given a new examScores that's directly assigned to the object student
+        student.examScores = new Double[]{100.0, 99.9, 99.8}; //<-FRAN IS A FANTASTIC STUDENT!!!
+
+        //When assigned as Arrays.toString(Convert array to string to be printed out)
+        String expected = Arrays.toString(student.examScores);
+        String actual = Arrays.toString(student.getExamScores());
+
+        //Then student.examScores should equal to student.getExamScores()
+        System.out.println("Initial examScores: " + expected + " equals to " + actual);
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void addExamScoresTest() {
+        //Given a new examScores
+        getExamScoresTest(); //<-Grabbing the exams added from getExamScoresTest
+        student.addExamScores(52.4); //<-Fran is playing too much DodgeBall and not enough studying
+        student.addExamScores(90.5); //<-Multiple tests to make sure all the scores are added
+        student.addExamScores(10.0); //<-Fran is addicted to DodgeBall
+
+        //When assigned as Arrays.toString(convert array to string to be printed out)
+        String expected = Arrays.toString(student.examScores);
+        String actual = Arrays.toString(student.getExamScores());
+
+        //Then student.examScores should equal to student.getExamScores() method with the new multiple addExamScores() value to prove it works
+        System.out.println("Adding new examScores: " + expected + " equals to " + actual);
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void getNumberOfExamsTakenTest() {
+        //Given examScores from the previous test method
+        addExamScoresTest();
+        student.addExamScores(99.9); //<-Fran has finally decided to study harder instead of focusing on DodgeBall, she's still deadly though
+
+        //When getNumberOfExamsTaken() method is called
+        int expected = student.examScores.length;
+        int actual = student.getNumberOfExamsTaken();
+
+        //Then examScores.length should equal to student.getNumberOfExamsTaken() method
+        System.out.println("Adding another examScore: " + Arrays.toString(student.getExamScores()));
+        System.out.println("Number of examsTaken: " + expected + " equals to " + actual);
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void setExamScoresTest() {
+        //Given examScores from the previous test method
+        getNumberOfExamsTakenTest();
+
+        //When setExamScores() method is called
+        String actual = Arrays.toString(student.setExamScores(6, 90.5)); //<- Fran has decided to retake her terrible exam and aced it
+        String expected = Arrays.toString(student.getExamScores());
+
+        //Then student.getExamScores() method should equal to the newly updated student.setExamScores() method
+        System.out.println("Set index 6-1 with the new examScore: " + expected + " equals to " + actual);
+        Assert.assertEquals(expected,actual);
     }
 }
